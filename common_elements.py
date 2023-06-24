0767d7f5011a1a3629f978003b3a693083a37420
ElonTusk404 <igmaflappy@gmail.com>
2023-06-24 18:13:08 +0400

    Update common_elements.py

--- common_elements.py
+++ common_elements.py
@@ -3,4 +3,4 @@ def find_common_elements(list1, list2):
     for element in list1:
         if element in list2:
             common_list.append(element)
-    return common_list
+    return list(set(common_list))
